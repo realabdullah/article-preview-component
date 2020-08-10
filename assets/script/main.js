5123932156c33775786313cbd738f1cfd7b0e04a
Abdullahi Odesanmi <56407386+realabdullah@users.noreply.github.com>
2020-08-10 16:26:43 -0700

    Add files via upload

---
+++ assets/script/main.js
@@ -0,0 +1,13 @@
+function showShare() {
+    const share = document.getElementById("active");
+    if (share.style.display === "block") {
+        share.style.display = "none";
+    } else {
+        share.style.display = "block"
+    }
+}
+
+function showShar() {
+    var popup = document.getElementById("active");
+    popup.classList.toggle("show");
+}
